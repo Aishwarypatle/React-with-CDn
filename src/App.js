843ac11c41454a0d1501ba3ca2d00915df113460
Your Name <you@example.com>
2024-03-08 14:21:18 +0530

    react app made using cdn

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Exporting the App component
+function App() {
+    return (
+        <div className="appjs">
+            <h1>Hello, React!</h1>
+            <p>This is a React app created using CDN with JS files in src directory.</p>
+        </div>
+    );
+}
+
+export default App;
